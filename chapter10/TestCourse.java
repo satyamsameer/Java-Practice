ff79e9b29576f8b21baf92e1a8a97bb861cb4cc2
Sameer Satyam <sameersatyam01@gmail.com>
2017-01-20 00:00:59 +0530

    ch10 program 3

---
+++ chapter10/TestCourse.java
@@ -0,0 +1,17 @@
+public class TestCourse{
+	public static void main(String[] args){
+		Course course1=new Course("Data Structures");
+		Course course2-new Course("Database Systems");
+		
+		course1.addStudent("Peter Jones");
+		course1.addStudent("Kin Smith");
+		course1.addStudent("Anne Kennedy");
+		
+		course2.addStudent("Peter Jones");
+		course2.addStudent("Steve Smith");
+		
+		System.out.println("Number of students in course1: "+course1.getNumberOfStudents());
+		
+		String[] students=course1.getStudents();
+	}
+}
