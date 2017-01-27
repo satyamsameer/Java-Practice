e329f0ee2c0ec1e9d5ab301257bcff51443c144b
Sameer Satyam <sameersatyam01@gmail.com>
2017-01-28 01:26:28 +0530

    ch12 program 2

---
+++ chapter12/QuotientWithIf.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class QuotientWithIf{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter the two integers: ");
+		
+		int num1=input.nextInt();
+		int num2=input.nextInt();
+		if(num2!=0){
+			System.out.println(num1+" / "+num2+" is "+(num1/num2));
+		}else{
+			System.out.println("Divisor cannot be zero ");
+		}
+		
+	}
+}
