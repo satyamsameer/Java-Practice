21a202745c8d2a514249a4f6e951f986f3e2539e
Sameer Satyam <sameersatyam01@gmail.com>
2017-02-05 13:59:24 +0530

    ch12 program 11

---
+++ chapter12/WriteData.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+public class WriteData{
+	public static void main(String args[]) throws IOException{
+		java.io.File file=new java.io.File("score.txt");
+		if(file.exists()){
+			System.out.println("File already exists");
+			System.exit(1);
+		}
+		java.io.PrintWriter output=new java.io.PrintWriter(file);
+		output.print("John T Smith ");
+		output.println(90);
+		output.print("Eric K Jones ");
+		output.println(85);
+		
+		output.close();
+	}
+}
