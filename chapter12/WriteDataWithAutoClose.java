56ba199c49e0d050d8d511b7dac40823c390aa70
Sameer Satyam <sameersatyam01@gmail.com>
2017-02-15 23:47:02 +0530

    ch12 program 12

---
+++ chapter12/WriteDataWithAutoClose.java
@@ -0,0 +1,17 @@
+public class WriteDataWithAutoClose{
+	public static void main(String[] args) throws Exception{
+		java.io.File file=new java.io.File("scores.txt");
+		if(file.exists()){
+			System.out.println("File alreday exists");
+			System.exit(0);
+		}
+		try(
+			java.io.PrintWriter output=new java.io.PrintWriter(file);
+		){
+			output.print("John T Smith");
+			output.println(90);
+			output.print("Eric K Jones");
+			output.println(85);
+		}
+	}
+}
