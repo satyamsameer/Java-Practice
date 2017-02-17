6c982e59b3dd469a3d54df1a205d3b1e1e5aa385
Sameer Satyam <sameersatyam01@gmail.com>
2017-02-17 23:53:01 +0530

    ch12 program 16

---
+++ chapter12/WebCrawler.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class WebCrawler{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter the URL: ");
+		String url=input.nextLine();
+		crawler(url);
+	}
+	public static void crawler(String startingURL){
+		ArrayList<String> listOfPendingURLs=new ArrayList<>();
+		ArrayList<String> listOfTraversedURLs=new ArrayList<>();
+		
+		String urlString="";
+		
+		listOfPendingURLs.add(startingURL);
+		while(!listOfPendingURLs.isEmpty()&&listOfTraversedURLs.size()<=100){
+			urlString=listOfPendingURLs.remove(0);
+			if(!listOfTraversedURLs.contains(urlString)){
+				listOfTraversedURLs.add(urlString);
+				System.out.println("Crawl: "+urlString);
+				
+				for(String s: getSubURLs(urlString)){
+					if(!listOfTraversedURLs.contains(s)){
+						listOfPendingURLs.add(s);
+					}
+				}
+			}
+		}
+	}
+	
+	public static ArrayList<String> getSubURLs(String urlString){
+		ArrayList<String> list=new ArrayList<>();
+		try{
+			java.net.URL url=new java.net.URL(urlString);
+			Scanner input=new Scanner(url.openStream());
+			int current=0;
+			while(input.hasNext()){
+				String line=input.nextLine();
+				current=line.indexOf("http:",current);
+				while(current>0){
+					int endIndex=line.indexOf("\"",current);
+					if(endIndex>0){
+						list.add(line.substring(current,endIndex));
+						current=line.indexOf("http:",endIndex);
+					}else{
+						current=-1;
+					}
+				}
+			}
+		}catch(Exception ex){
+			System.out.println("Error: "+ex.getMessage());
+		}
+		return list; 
+	}
+}
